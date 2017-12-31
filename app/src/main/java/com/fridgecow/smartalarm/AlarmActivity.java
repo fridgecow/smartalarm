05082165c21bcb0ceff5203d74bf5b9fc82de74a
Tom Benn <tom@fridgecow.com>
2017-12-31 18:32:33 +0000

    Implemented snoozing and added a configuration option

--- app/src/main/java/com/fridgecow/smartalarm/AlarmActivity.java
+++ app/src/main/java/com/fridgecow/smartalarm/AlarmActivity.java
@@ -1,16 +1,24 @@
 package com.fridgecow.smartalarm;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.support.wearable.activity.WearableActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AlarmActivity extends WearableActivity {
 
     private TextView mTime;
     private Button mDone;
+    private Button mSnooze;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +26,31 @@ public class AlarmActivity extends WearableActivity {
         setContentView(R.layout.activity_alarm);
 
         mDone = (Button) findViewById(R.id.button3);
+        mSnooze = findViewById(R.id.button5);
+
         mDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                vibrator.cancel();
+
+                finish();
+            }
+        });
+        mSnooze.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent alarmIntent = new Intent(view.getContext(), AlarmActivity.class);
+
+                PendingIntent snoozeIntent = PendingIntent.getActivity(view.getContext(),  0, alarmIntent, 0);
+                AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(view.getContext());
+                int snoozeTime = prefs.getInt("smartalarm_snooze", 5)*60*1000;
+
+                alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + snoozeTime, snoozeIntent);
+
+                //Finish for now
                 Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
                 vibrator.cancel();
 
