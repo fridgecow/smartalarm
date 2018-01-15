751e8d9c112d648d1fc8306c3d8d825bd0e5643e
Tom Benn <tom@fridgecow.com>
2018-01-15 18:09:18 +0000

    Fixed settings access and added a 'done' button to changelog

--- app/src/main/java/com/fridgecow/smartalarm/ChangelogActivity.java
+++ app/src/main/java/com/fridgecow/smartalarm/ChangelogActivity.java
@@ -2,18 +2,27 @@ package com.fridgecow.smartalarm;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ChangelogActivity extends WearableActivity {
 
-    private TextView mTextView;
+    private Button mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_changelog);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        mTextView = findViewById(R.id.changelog_done);
+
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         // Enables Always-on
         setAmbientEnabled();
